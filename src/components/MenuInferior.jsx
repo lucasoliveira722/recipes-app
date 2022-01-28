8acbcf339f4d2351f52ba21ec58bb0bb07883318
Luis Gustavo <l.gustavomelo@hotmail.com>
2022-01-27 23:26:49 -0300

    feat:Requisito 22, 23 e 24 feitos

--- src/components/MenuInferior.jsx
+++ src/components/MenuInferior.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import mealIcon from '../images/mealIcon.svg';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 
 export default function MenuInferior() {
+  const history = useHistory();
+
   return (
     <footer
       data-testid="footer"
@@ -12,6 +15,7 @@ export default function MenuInferior() {
         type="button"
         data-testid="food-bottom-btn"
         src={ mealIcon }
+        onClick={ () => history.push('/foods') }
       >
         <img alt="Ícone de Comidas" src={ mealIcon } />
       </button>
@@ -20,14 +24,16 @@ export default function MenuInferior() {
         type="button"
         data-testid="drinks-bottom-btn"
         src={ drinkIcon }
+        onClick={ () => history.push('/drinks') }
       >
         <img alt="Ícone de Bebidas" src={ drinkIcon } />
       </button>
+
       <button
         type="button"
         data-testid="explore-bottom-btn"
         src={ exploreIcon }
-        // onClick={}
+        onClick={ () => history.push('/explore') }
       >
         <img alt="Ícone de Exploração" src={ exploreIcon } />
       </button>
