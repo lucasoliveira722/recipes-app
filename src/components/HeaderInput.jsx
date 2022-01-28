9cafe7efe465b64ebc3825812de7e385a6f20804
Douglas Ialamov <douglas.ialamov@gmail.com>
2022-01-28 15:07:10 -0300

    feat: Req. 13 done.

--- src/components/HeaderInput.jsx
+++ src/components/HeaderInput.jsx
@@ -6,8 +6,45 @@ export default function HeaderInput() {
       <input
         type="text"
         data-testid="search-input"
+        placeholder="Search Recipe"
         name="search"
       />
+      <label htmlFor="ingredient">
+        <input
+          type="radio"
+          data-testid="ingredient-search-radio"
+          id="ingredient"
+          name="ingredient"
+          value="ingredient"
+        />
+        Ingrediet
+      </label>
+      <label htmlFor="name">
+        <input
+          type="radio"
+          data-testid="name-search-radio"
+          id="name"
+          name="name"
+          value="name"
+        />
+        Name
+      </label>
+      <label htmlFor="first-letter">
+        <input
+          type="radio"
+          data-testid="first-letter-search-radio"
+          id="first-letter"
+          name="first-letter"
+          value="first-letter"
+        />
+        First Letter
+      </label>
+      <button
+        type="button"
+        data-testid="exec-search-btn"
+      >
+        Search
+      </button>
     </div>
   );
 }
