9b5aa6fc3958f581c71900c6f7b3e77869cf2306
Pedro Ivo <pedroivobrandaomattos@gmail.com>
2022-01-31 16:21:36 -0300

    Adding buttons categories to drinks

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -3,14 +3,23 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import CardRecipe from '../components/CardRecipe';
 import AppContext from '../context/AppContext';
+import ButtonsRecipe from '../components/ButtonsRecipe';
 
 export default function Drinks() {
   const urlDrinks = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
-  const { drinkRecipes, setDrinkRecipes, getRecipes } = useContext(AppContext);
+  const urlDrinkCategory = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+  const { drinkRecipes,
+    setDrinkRecipes,
+    getRecipes,
+    drinkCategories,
+    setDrinkCategories,
+    getCategories } = useContext(AppContext);
   useEffect(() => {
     (async () => {
       const { drinks } = await getRecipes(urlDrinks);
       setDrinkRecipes(drinks);
+      const categories = await getCategories(urlDrinkCategory);
+      setDrinkCategories(categories.drinks);
     })();
   }, []);
   console.log(drinkRecipes);
@@ -18,6 +27,7 @@ export default function Drinks() {
   return (
     <div>
       <Header title="Drinks" />
+      <ButtonsRecipe categoriesNames={ drinkCategories } />
       {drinkRecipes.slice(0, maxRecipes)
         .map((recipe, index) => (
           <CardRecipe
