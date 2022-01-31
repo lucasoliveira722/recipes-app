45c6c8aa34a8b7ffcfe7f755b89ca0c8395b9ff9
Pedro Ivo <pedroivobrandaomattos@gmail.com>
2022-01-31 15:27:26 -0300

    Add return in CardRecipe and PropTypes

--- src/components/CardRecipe.jsx
+++ src/components/CardRecipe.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function CardRecipe({ index, url, name }) {
-  <div data-testid={ `${index}-recipe-card` }>
-    <img src={ url } alt="" data-testid={ `${index}-card-img` } />
-    <span data-testid={ `${index}-card-name` }>{name}</span>
-  </div>;
+  return (
+    <div data-testid={ `${index}-recipe-card` }>
+      <img src={ url } alt="" data-testid={ `${index}-card-img` } />
+      <span data-testid={ `${index}-card-name` }>{name}</span>
+    </div>
+  );
 }
+
+CardRecipe.propTypes = {
+  index: PropTypes.number,
+  url: PropTypes.string,
+  name: PropTypes.string,
+}.isRequired;
