e0e99efa849d9d6f53264ed513db460432e58c9c
Pedro Ivo <pedroivobrandaomattos@gmail.com>
2022-01-31 15:33:14 -0300

    Refactoring searchCategotyRecipe

--- src/services/searchCategoryRecipe.js
+++ src/services/searchCategoryRecipe.js
@@ -1,21 +1,7 @@
-const urlFood = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
-const urlDrinks = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
-
-const searchCategoryRecipe = async (type) => {
-  const getData = async (url) => {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-  };
-  switch (type) {
-  case 'foods':
-    getData(urlFood);
-    break;
-  case 'drinks':
-    getData(urlDrinks);
-    break;
-  default:
-  }
+const searchCategoryRecipe = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return response.ok ? Promise.resolve(data) : Promise.reject(data);
 };
 
 export default searchCategoryRecipe;
