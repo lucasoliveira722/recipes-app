719a898213e99d00f97b3cf7819f01dac544e78e
Pedro Ivo <pedroivobrandaomattos@gmail.com>
2022-01-31 15:24:25 -0300

    Add some states and function in provider

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
+import searchRecipes from '../services/searchRecipesApi';
 import AppContext from './AppContext';
 
 function Provider({ children }) {
@@ -8,6 +9,12 @@ function Provider({ children }) {
   const [enable, setEnable] = useState(true);
   const [searchField, setSearchField] = useState('ingredient');
   const [search, setSearch] = useState('ingredient');
+  const [drinkRecipes, setDrinkRecipes] = useState([]);
+  const [foodRecipes, setFoodRecipes] = useState([]);
+  const getRecipes = async (url) => {
+    const result = await searchRecipes(url);
+    return result;
+  };
   const contextValue = {
     email,
     password,
@@ -19,6 +26,11 @@ function Provider({ children }) {
     setEnable,
     setSearch,
     setSearchField,
+    getRecipes,
+    drinkRecipes,
+    setDrinkRecipes,
+    foodRecipes,
+    setFoodRecipes,
   };
 
   return (
