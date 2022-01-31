3777dc7d980afc0ac84a575e55a0eb60284d030e
Luis Gustavo <l.gustavomelo@hotmail.com>
2022-01-31 16:37:36 -0300

    feat(exploreFoodsDrinks): requirements 70 a 73

--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 export default function ExploreDrinks() {
+  const history = useHistory();
   return (
     <>
       <Header title="Explore Drinks" />
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explore/drinks/ingredients') }
+      >
+        By Ingredient
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        // onClick={ }
+      >
+        Surprise me!
+      </button>
       <Footer />
     </>
   );
--- src/pages/ExploreFoods.jsx
+++ src/pages/ExploreFoods.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 export default function ExploreFoods() {
+  const history = useHistory();
   return (
     <>
       <Header title="Explore Foods" />
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explore/foods/ingredients') }
+      >
+        By Ingredient
+      </button>
+      <button
+        type="button"
+        data-testid="explore-by-nationality"
+        onClick={ () => history.push('/explore/foods/nationalities') }
+      >
+        By Nationality
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        // onClick={ }
+      >
+        Surprise me!
+      </button>
       <Footer />
     </>
   );
