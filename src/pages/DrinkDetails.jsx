0dc3a3445a5bdf3c5248add4c67f12333aa9bb0e
Pedro Ivo <pedroivobrandaomattos@gmail.com>
2022-02-01 20:21:27 -0300

    Initial steps in page details

--- src/pages/DrinkDetails.jsx
+++ src/pages/DrinkDetails.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import searchByIdRecipe from '../services/searchByIdRecipe';
 
-export default function DrinkDetails() {
+export default function DrinkDetails({ match: { params: { id } } }) {
+  const [detailsId, setDetailsId] = useState({});
+  useEffect(() => {
+    (async () => {
+      const result = await searchByIdRecipe(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+      const details = result.drinks;
+      setDetailsId(details[0]);
+    })();
+  }, []);
+  console.log(detailsId);
+  const filter1 = Object.entries(detailsId);
+  const ingredientKey = 'strIngredient';
+  const filter2 = filter1.filter((array) => (
+    array[0].toUpperCase().includes(ingredientKey.toUpperCase())));
   return (
     <div>
-      <h1 data-testid="recipe-category">Category</h1>
-      <h1 data-testid="recipe-title">Title</h1>
-      <img src="" alt="recipephoto" data-testid="recipe-photo" />
+      <h1 data-testid="recipe-category">{detailsId.strCategory}</h1>
+      <h1 data-testid="recipe-title">{detailsId.strDrink}</h1>
+      <img src={ detailsId.strDrinkThumb } alt="recipephoto" data-testid="recipe-photo" />
       <button
         data-testid="share-btn"
         type="button"
@@ -19,10 +34,18 @@ export default function DrinkDetails() {
         FavoriteBtn
       </button>
       <ul>
-        <li data-testid="0-ingredient-name-and-measure">Ingerdient</li>
+        {filter2.map((arr, index) => (
+          arr[1] !== null && (
+            <li
+              key={ arr[1] }
+              data-testid={ `${index}-ingredient-name-and-measure` }
+            >
+              {arr[1]}
+            </li>
+          )
+        ))}
       </ul>
-      <p data-testid="instructions">Instructions</p>
-      <video data-testid="video"><track src="" /></video>
+      <p data-testid="instructions">{detailsId.strInstructions}</p>
       <ul>
         <li data-testid="0-recomedation-card">Receitas recomendadas</li>
       </ul>
@@ -35,3 +58,7 @@ export default function DrinkDetails() {
     </div>
   );
 }
+
+DrinkDetails.propTypes = {
+  match: PropTypes.objectOf,
+}.isRequired;
--- src/pages/FoodDetails.jsx
+++ src/pages/FoodDetails.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import searchByIdRecipe from '../services/searchByIdRecipe';
 
-export default function FoodDetails() {
+export default function FoodDetails({ match: { params: { id } } }) {
+  const [detailsId, setDetailsId] = useState({});
+  useEffect(() => {
+    (async () => {
+      const result = await searchByIdRecipe(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+      const details = result.meals;
+      setDetailsId(details[0]);
+    })();
+  }, []);
+  console.log(detailsId);
+  const filter1 = Object.entries(detailsId);
+  const ingredientKey = 'strIngredient';
+  const filter2 = filter1.filter((array) => (
+    array[0].toUpperCase().includes(ingredientKey.toUpperCase())));
+  const video = detailsId.strYoutube.replace('watch?v', 'embed/');
   return (
     <div>
-      <h1 data-testid="recipe-category">Category</h1>
-      <h1 data-testid="recipe-title">Title</h1>
-      <img src="" alt="recipephoto" data-testid="recipe-photo" />
+      <h1 data-testid="recipe-category">{detailsId.strCategory}</h1>
+      <h1 data-testid="recipe-title">{detailsId.strMeal}</h1>
+      <img src={ detailsId.strMealThumb } alt="recipephoto" data-testid="recipe-photo" />
       <button
         data-testid="share-btn"
         type="button"
@@ -19,10 +35,27 @@ export default function FoodDetails() {
         FavoriteBtn
       </button>
       <ul>
-        <li data-testid="0-ingredient-name-and-measure">Ingerdient</li>
+        {filter2.map((arr, index) => (
+          arr[1] !== '' && (
+            <li
+              key={ arr[1] }
+              data-testid={ `${index}-ingredient-name-and-measure` }
+            >
+              {arr[1]}
+            </li>
+          )
+        ))}
       </ul>
-      <p data-testid="instructions">Instructions</p>
-      <video data-testid="video"><track src="" /></video>
+      <p data-testid="instructions">{detailsId.strInstructions}</p>
+      {/* Referencia para o uso do iframe */}
+      {/* https://www.w3schools.com/html/html_iframe.asp */}
+      <iframe
+        src={ video }
+        frameBorder="0"
+        allowFullScreen
+        title="video"
+        data-testid="video"
+      />
       <ul>
         <li data-testid="0-recomedation-card">Receitas recomendadas</li>
       </ul>
@@ -35,3 +68,7 @@ export default function FoodDetails() {
     </div>
   );
 }
+
+FoodDetails.propTypes = {
+  match: PropTypes.objectOf,
+}.isRequired;
