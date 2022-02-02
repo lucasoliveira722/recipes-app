ae247f860ff8cd10183b35a847a214ab37e088c9
Pedro Ivo <pedroivobrandaomattos@gmail.com>
2022-02-02 12:15:19 -0300

    Reqs 33 to 36 done

--- src/pages/DrinkDetails.jsx
+++ src/pages/DrinkDetails.jsx
@@ -1,26 +1,50 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import searchByIdRecipe from '../services/searchByIdRecipe';
+import searchRecipes from '../services/searchRecipesApi';
 
 export default function DrinkDetails({ match: { params: { id } } }) {
   const [detailsId, setDetailsId] = useState({});
+  const [recommended, setRecommended] = useState([]);
   useEffect(() => {
     (async () => {
       const result = await searchByIdRecipe(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
       const details = result.drinks;
       setDetailsId(details[0]);
+      const resultRe = await searchRecipes('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+      const recommends = resultRe.meals;
+      setRecommended(recommends);
     })();
   }, []);
   console.log(detailsId);
   const filter1 = Object.entries(detailsId);
   const ingredientKey = 'strIngredient';
-  const filter2 = filter1.filter((array) => (
+  const filter2Ingredient = filter1.filter((array) => (
     array[0].toUpperCase().includes(ingredientKey.toUpperCase())));
+  const measureKey = 'strMeasure';
+  const filter2Measure = filter1.filter((array) => (
+    array[0].toUpperCase().includes(measureKey.toUpperCase())));
+  const mapFilter = (array) => array.map((arr, index) => (
+    (arr[1] !== '' && arr[1] !== null && arr[1] !== ' ') && (
+      <li
+        key={ `${arr[1]}${arr[0]}` }
+        data-testid={ `${index}-ingredient-name-and-measure` }
+      >
+        {arr[1]}
+      </li>
+    )
+  ));
+  const maxRecommends = 6;
   return (
     <div>
-      <h1 data-testid="recipe-category">{detailsId.strCategory}</h1>
+      <h1 data-testid="recipe-category">{detailsId.strAlcoholic}</h1>
       <h1 data-testid="recipe-title">{detailsId.strDrink}</h1>
-      <img src={ detailsId.strDrinkThumb } alt="recipephoto" data-testid="recipe-photo" />
+      <img
+        style={ { width: 200 } }
+        src={ detailsId.strDrinkThumb }
+        alt="recipephoto"
+        data-testid="recipe-photo"
+      />
       <button
         data-testid="share-btn"
         type="button"
@@ -34,21 +58,45 @@ export default function DrinkDetails({ match: { params: { id } } }) {
         FavoriteBtn
       </button>
       <ul>
-        {filter2.map((arr, index) => (
-          arr[1] !== null && (
-            <li
-              key={ arr[1] }
-              data-testid={ `${index}-ingredient-name-and-measure` }
-            >
-              {arr[1]}
-            </li>
-          )
-        ))}
+        {mapFilter(filter2Ingredient)}
+      </ul>
+      <ul>
+        {mapFilter(filter2Measure)}
       </ul>
       <p data-testid="instructions">{detailsId.strInstructions}</p>
-      <ul>
-        <li data-testid="0-recomedation-card">Receitas recomendadas</li>
-      </ul>
+      <div
+        style={ {
+          width: 450,
+          height: 200,
+          whiteSpace: 'nowrap',
+          overflow: 'scroll',
+        } }
+      >
+        {recommended.slice(0, maxRecommends)
+          .map((recipe, index) => (
+            <button
+              data-testid={ `${index}-recomendation-card` }
+              style={ { display: 'inline-block', width: 200 } }
+              type="button"
+              key={ index }
+            >
+              <div>
+                <img
+                  style={ { width: 180, height: 150 } }
+                  src={ recipe.strMealThumb }
+                  alt="recipe"
+                />
+                <span
+                  style={ { display: 'block' } }
+                  data-testid={ `${index}-recomedation-title` }
+                >
+                  {recipe.strMeal}
+                </span>
+
+              </div>
+            </button>
+          ))}
+      </div>
       <button
         type="button"
         data-testid="start-recipe-btn"
--- src/pages/FoodDetails.jsx
+++ src/pages/FoodDetails.jsx
@@ -1,27 +1,55 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import searchByIdRecipe from '../services/searchByIdRecipe';
+import searchRecipes from '../services/searchRecipesApi';
 
 export default function FoodDetails({ match: { params: { id } } }) {
   const [detailsId, setDetailsId] = useState({});
+  const [recommended, setRecommended] = useState([]);
+  // const [video, setVideo] = useState('');
   useEffect(() => {
     (async () => {
-      const result = await searchByIdRecipe(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
-      const details = result.meals;
+      const resultId = await searchByIdRecipe(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+      const details = resultId.meals;
       setDetailsId(details[0]);
+      const resultRe = await searchRecipes('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+      const recommends = resultRe.drinks;
+      setRecommended(recommends);
     })();
   }, []);
   console.log(detailsId);
   const filter1 = Object.entries(detailsId);
   const ingredientKey = 'strIngredient';
-  const filter2 = filter1.filter((array) => (
+  const filter2Ingredient = filter1.filter((array) => (
     array[0].toUpperCase().includes(ingredientKey.toUpperCase())));
-  const video = detailsId.strYoutube.replace('watch?v', 'embed/');
+  const measureKey = 'strMeasure';
+  const filter2Measure = filter1.filter((array) => (
+    array[0].toUpperCase().includes(measureKey.toUpperCase())));
+  const mapFilter = (array) => array.map((arr, index) => (
+    (arr[1] !== '' && arr[1] !== null && arr[1] !== ' ') && (
+      <li
+        key={ `${arr[1]}${arr[0]}` }
+        data-testid={ `${index}-ingredient-name-and-measure` }
+      >
+        {arr[1]}
+      </li>
+    )
+  ));
+  const maxRecommends = 6;
+  // if (detailsId.strYoutube !== '') {
+  //   setVideo(detailsId.strYoutube.replace('watch?v', 'embed/'));
+  // }
+
   return (
     <div>
       <h1 data-testid="recipe-category">{detailsId.strCategory}</h1>
       <h1 data-testid="recipe-title">{detailsId.strMeal}</h1>
-      <img src={ detailsId.strMealThumb } alt="recipephoto" data-testid="recipe-photo" />
+      <img
+        style={ { width: 200 } }
+        src={ detailsId.strMealThumb }
+        alt="recipephoto"
+        data-testid="recipe-photo"
+      />
       <button
         data-testid="share-btn"
         type="button"
@@ -35,30 +63,54 @@ export default function FoodDetails({ match: { params: { id } } }) {
         FavoriteBtn
       </button>
       <ul>
-        {filter2.map((arr, index) => (
-          arr[1] !== '' && (
-            <li
-              key={ arr[1] }
-              data-testid={ `${index}-ingredient-name-and-measure` }
-            >
-              {arr[1]}
-            </li>
-          )
-        ))}
+        {mapFilter(filter2Ingredient)}
+      </ul>
+      <ul>
+        {mapFilter(filter2Measure)}
       </ul>
       <p data-testid="instructions">{detailsId.strInstructions}</p>
       {/* Referencia para o uso do iframe */}
       {/* https://www.w3schools.com/html/html_iframe.asp */}
       <iframe
-        src={ video }
+        src={ detailsId.strYoutube }
         frameBorder="0"
         allowFullScreen
         title="video"
         data-testid="video"
       />
-      <ul>
-        <li data-testid="0-recomedation-card">Receitas recomendadas</li>
-      </ul>
+      <div
+        style={ {
+          width: 450,
+          height: 200,
+          whiteSpace: 'nowrap',
+          overflow: 'scroll',
+        } }
+      >
+        {recommended.slice(0, maxRecommends)
+          .map((recipe, index) => (
+            <button
+              data-testid={ `${index}-recomendation-card` }
+              style={ { display: 'inline-block', width: 200 } }
+              type="button"
+              key={ index }
+            >
+              <div>
+                <img
+                  style={ { width: 180, height: 150 } }
+                  src={ recipe.strDrinkThumb }
+                  alt="recipe"
+                />
+                <span
+                  style={ { display: 'block' } }
+                  data-testid={ `${index}-recomedation-title` }
+                >
+                  {recipe.strDrink}
+                </span>
+
+              </div>
+            </button>
+          ))}
+      </div>
       <button
         type="button"
         data-testid="start-recipe-btn"
