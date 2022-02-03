146ea7e3752419c35f29430284e561b830ed79d0
Luis Gustavo <l.gustavomelo@hotmail.com>
2022-02-03 19:50:57 -0300

    branch settings

--- src/pages/ExploreDrinksIngredients.jsx
+++ src/pages/ExploreDrinksIngredients.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import searchExploreIngredient from '../services/searchExploreIngredient';
 
 export default function ExploreDrinksIngredients() {
   return (
--- src/pages/ExploreFoodsIngredients.jsx
+++ src/pages/ExploreFoodsIngredients.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import searchExploreIngredient from '../services/searchExploreIngredient';
+// import { ingredientsFood } from '../services/searchExploreIngredient';
 
 export default function ExploreFoodsIngredients() {
   return (
